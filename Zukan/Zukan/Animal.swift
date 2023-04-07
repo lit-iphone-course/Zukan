5f4193bbdaecc839b4eb5aa68b2411cc74e53500
moyotsukai <gyoshu.bowie@gmail.com>
2023-04-07 20:27:53 +0900

    add: 動物図鑑

---
+++ Zukan/Zukan/Animal.swift
@@ -0,0 +1,26 @@
+//
+//  Animal.swift
+//  Zukan
+//
+//  Created by Owner on 2023/04/06.
+//
+
+import UIKit
+
+class Animal {
+    
+    var name: String
+    var details: String
+    var imageName: String
+    
+    init(name: String, details: String, imageName: String) {
+        self.name = name
+        self.details = details
+        self.imageName = imageName
+    }
+    
+    func getImage() -> UIImage {
+        print(imageName)
+        return UIImage(named: imageName)!
+    }
+}
--- Zukan/Zukan/ViewController.swift
+++ Zukan/Zukan/ViewController.swift
@@ -1,12 +1,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var detailsLabel: UILabel!
 
+    var animalArray: [Animal] = [
+        Animal(name: "キリン", details: "アフリカのサバンナに住んでいる。舌がとても長い。", imageName: "kirin.jpg"),
+        Animal(name: "キングペンギン", details: "南極の島々に住んでいる。魚類やイカなどを食べる。", imageName: "penguin.jpg"),
+        Animal(name: "カンガルー", details: "オーストラリア固有の動物。走るのが速い。", imageName: "kangaroo.jpg")
+    ]
+    var index: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUI()
     }
 
-
+    @IBAction func next() {
+        index = index + 1
+        setUI()
+    }
+    
+    @IBAction func back() {
+        index = index - 1
+        setUI()
+    }
+    
+    func setUI() {
+        imageView.image = animalArray[index].getImage()
+        nameLabel.text = animalArray[index].name
+        detailsLabel.text = animalArray[index].details
+    }
+    
 }
 
